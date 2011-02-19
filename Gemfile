54b2fd2bc488ea784deb0a47820d05f3ff651e67
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-02-18 20:59:53 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+gem 'mysql2'
+
+group :development, :test do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LandInfo::Application.config.secret_token = 'f7d7793d4674a0bc2ebce2b380c459cd0eeda08e6033af0eb16247d3503d6261f99dfe3290c12504b58cbd1c4678ae1d7a546147a9100604a90ebd9027baa5fd'
