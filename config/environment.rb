5c78b0f3ce17eefd2dc3b541e81ec64b08c72c69
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-02-28 16:05:46 -0700

    add sql server jdbc driver to lib folder and add it to CLASSPATH

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+$CLASSPATH << "file:///#{Rails.root}/lib/java/sqljdbc4.jar"
+
 # Initialize the rails application
 LandInfo::Application.initialize!
