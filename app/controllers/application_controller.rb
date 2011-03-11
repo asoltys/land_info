3a02db367978b0e138344f07070f7c83fc3a5732
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-03-11 13:22:21 -0700

    change default records per page to 60

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  ActiveScaffold.set_defaults do |config| 
+    config.ignore_columns.add [:created_at, :updated_at, :lock_version]
+    config.list.per_page = 60
+  end
+
   def index 
   end
 end
