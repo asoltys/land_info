deee33c085b356d261a2fc8c7148b3229c93eb69
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-03-16 15:46:53 -0600

    add mysql jdbc driver to classpath

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 unless $CLASSPATH.nil?
-  $CLASSPATH << "file:///#{Rails.root}/lib/java/sqljdbc4.jar"
+  $CLASSPATH << "file:///#{Rails.root}/lib/java/mysql-connector-java-5.1.15-bin.jar"
 end
 
 # Initialize the rails application
