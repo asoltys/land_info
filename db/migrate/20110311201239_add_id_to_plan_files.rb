edb7538516aff5e4ab94c8fd6d5644afc42ec0af
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-04-01 14:42:28 -0600

    don't need to add id anymore

--- db/migrate/20110311201239_add_id_to_plan_files.rb
+++
@@ -1,9 +0,0 @@
-class AddIdToPlanFiles < ActiveRecord::Migration
-  def self.up
-    add_column :plan_files, :id, :primary_key
-  end
-
-  def self.down
-    remove_column :plan_files, :id
-  end
-end
