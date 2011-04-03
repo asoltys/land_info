e3bba7c34eb295c5a7ff022b61b040b4a0053bae
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-04-03 12:49:07 -0600

    fix primary key syntax

--- db/migrate/001_create_database.rb
+++ db/migrate/001_create_database.rb
@@ -1,26 +1,26 @@
 class CreateDatabase < ActiveRecord::Migration
   def self.up
     if ActiveRecord::Base.connection.tables.include?("bclslist")
-      add_column :bclslist, :id, :primary_key => true
-      add_column :dwf_file, :id, :primary_key => true
-      add_column :featcode1, :id, :primary_key => true
-      add_column :featcode2, :id, :primary_key => true
-      add_column :fgo, :id, :primary_key => true
-      add_column :fgou, :id, :primary_key => true
-      add_column :lnd_dstrct1, :id, :primary_key => true
-      add_column :lnd_dstrct2, :id, :primary_key => true
-      add_column :marinefile, :id, :primary_key => true
-      add_column :nbc_loc1, :id, :primary_key => true
-      add_column :nbc_loc2, :id, :primary_key => true
-      add_column :nsurveyfiles1, :id, :primary_key => true
-      add_column :nsurveyfiles2, :id, :primary_key => true
-      add_column :prov_codes1, :id, :primary_key => true
-      add_column :prov_codes2, :id, :primary_key => true
-      add_column :region_bdy, :id, :primary_key => true
-      add_column :sf_tracking, :id, :primary_key => true
-      add_column :surv_pl, :id, :primary_key => true
-      add_column :survey_pln_title, :id, :primary_key => true
-      add_column :totscan, :id, :primary_key => true
+      add_column :bclslist, :id, :primary_key
+      add_column :dwf_file, :id, :primary_key
+      add_column :featcode1, :id, :primary_key
+      add_column :featcode2, :id, :primary_key
+      add_column :fgo, :id, :primary_key
+      add_column :fgou, :id, :primary_key
+      add_column :lnd_dstrct1, :id, :primary_key
+      add_column :lnd_dstrct2, :id, :primary_key
+      add_column :marinefile, :id, :primary_key
+      add_column :nbc_loc1, :id, :primary_key
+      add_column :nbc_loc2, :id, :primary_key
+      add_column :nsurveyfiles1, :id, :primary_key
+      add_column :nsurveyfiles2, :id, :primary_key
+      add_column :prov_codes1, :id, :primary_key
+      add_column :prov_codes2, :id, :primary_key
+      add_column :region_bdy, :id, :primary_key
+      add_column :sf_tracking, :id, :primary_key
+      add_column :surv_pl, :id, :primary_key
+      add_column :survey_pln_title, :id, :primary_key
+      add_column :totscan, :id, :primary_key
     else 
       create_table "bclslist" do |t|
         t.string "LAST_NAME"
