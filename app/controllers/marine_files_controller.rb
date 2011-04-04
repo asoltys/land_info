10f639a75f2e5be1b09b887db3718c8a488db53f
Michael Rybacha <michael.rybacha@pwgsc.gc.ca>
2011-04-04 09:31:08 -0700

    commit to be able to pull

--- app/controllers/marine_files_controller.rb
+++ app/controllers/marine_files_controller.rb
@@ -1,5 +1,7 @@
 class MarineFilesController < ApplicationController
   active_scaffold :marine_file do |conf|
+		conf.label = "Marine Files"
     conf.list.columns = [:file_number]
+		columns[:file_number].description = "Marine File Number"
   end
 end 
--- app/controllers/survey_files_controller.rb
+++ app/controllers/survey_files_controller.rb
@@ -1,5 +1,9 @@
 class SurveyFilesController < ApplicationController
   active_scaffold :survey_file do |conf|
+		conf.label = "Survey Files"
     conf.list.columns = [:survey_file, :start_date, :completion_date]
+		columns[:survey_file].description = "Survey File Number"
+		columns[:start_date].description = "Start Date (MM/DD/YYYY)"
+		columns[:completion_date].description = "Completion Date (MM/DD/YYYY)"
   end
 end 
