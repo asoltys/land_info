b5636cae15e35f4b43a21fe38e29ed05b03564aa
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-04-04 15:23:46 -0600

    link to plan files' tiff files on gtisdev if they exist

--- app/controllers/plan_files_controller.rb
+++ app/controllers/plan_files_controller.rb
@@ -1,7 +1,7 @@
 class PlanFilesController < ApplicationController
   active_scaffold :plan_file do |conf|
 		conf.label = "Plan Files"
-    conf.list.columns = [:plan_number, :dfrp_number, :location_number, :survey_file]
+    conf.list.columns = [:plan_number, :dfrp_number, :location_number, :survey_file, :tiff_file]
 		columns[:plan_number].description = "Plan Number"
 		columns[:dfrp_number].description = "DFRP Number"
 		columns[:location_number].description = "Location Number"
