b956284eebfc3f27371099411246cd89d401349c
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-04-05 16:54:27 -0600

    move custom active scaffold css into separate override file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   ActiveScaffold.set_defaults do |config| 
     config.ignore_columns.add [:created_at, :updated_at, :lock_version]
     config.list.per_page = 60
+    config.show.link = ''
+    config.delete.link = ''
+    config.update.link = ''
   end
 
   def index 
