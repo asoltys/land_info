f4f4129cfe1c7ec2c6e170a501092090232f8f9e
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-04-05 19:40:33 -0600

    display survey file description field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
   ActiveScaffold.set_defaults do |config| 
     config.ignore_columns.add [:created_at, :updated_at, :lock_version]
     config.list.per_page = 60
-    config.show.link = ''
-    config.delete.link = ''
-    config.update.link = ''
   end
 
   def index 
