b9995abb37eebd8493e89ddff189acc187d9d92c
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-04-05 21:40:56 -0600

    using more sensible columns on list pages and creating links

--- app/controllers/marine_files_controller.rb
+++ app/controllers/marine_files_controller.rb
@@ -1,6 +1,7 @@
 class MarineFilesController < ApplicationController
   active_scaffold :marine_file do |conf|
 		conf.label = "Marine Files"
-    conf.list.columns = [:file_number]
+    conf.list.columns = [:file_number, :reservation_use, :reservation_term, :Expiry, :Notes]
+    conf.columns[:file_number].set_link(:update)
   end
 end 
