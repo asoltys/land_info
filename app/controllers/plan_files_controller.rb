d81b42edc8fbd4d438bf04dfcfab819646ac81f3
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-04-05 19:50:39 -0600

    include more useful plan file fields on list page

--- app/controllers/plan_files_controller.rb
+++ app/controllers/plan_files_controller.rb
@@ -1,7 +1,7 @@
 class PlanFilesController < ApplicationController
   active_scaffold :plan_file do |conf|
 		conf.label = "Plan Files"
-    conf.list.columns = [:plan_number, :dfrp_number, :location_number, :survey_file, :tiff_file]
+    conf.list.columns = [:plan_number, :size, :title, :tiff_file]
   end
 end 
 
