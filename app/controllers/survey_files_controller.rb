f0c0b8e2d88e22af14b328266fd466233014e5e8
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-02-23 22:16:08 -0700

    Refine survey file fields

--- app/controllers/survey_files_controller.rb
+++ app/controllers/survey_files_controller.rb
@@ -3,13 +3,29 @@ class SurveyFilesController < ApplicationController
 
   active_scaffold :survey_file do |config|
 		config.label = "Survey Files"
+
     config.list.columns = [:survey_file, :description, :location]
-    config.columns = [:description, :location_id, :edrm_number, :start_date]
+
+    config.columns = [
+      :survey_file,
+      :description, 
+      :location_id, 
+      :edrm_number, 
+      :sigma_number,
+      :start_date,
+      :completion_date,
+      :active
+    ]
+
+    config.columns[:survey_file].set_link(:edit)
+
 		columns[:start_date].description = "(MM/DD/YYYY)"
 		columns[:completion_date].description = "(MM/DD/YYYY)"
-    config.columns[:survey_file].set_link(:edit)
 
     config.actions << :field_search
     config.actions.exclude :nested
+
+    config.list.sorting = { :survey_file => :desc }
+    config.list.always_show_search = true
   end
 end 
