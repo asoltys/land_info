acae112ad08f6efe7117ca1b472af160c71e8e04
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-02-29 21:17:17 -0700

    Specify model relationships

--- app/models/plan_file.rb
+++ app/models/plan_file.rb
@@ -1,5 +1,6 @@
 class PlanFile < ActiveRecord::Base
   belongs_to :location
+  belongs_to :survey_file
   has_many :drawings
 
   def to_label
--- app/models/survey_file.rb
+++ app/models/survey_file.rb
@@ -1,5 +1,6 @@
 class SurveyFile < ActiveRecord::Base
   belongs_to :location
+  has_many :plan_files
 
   def to_label
     "Survey File ##{survey_file}"
