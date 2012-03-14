09c62b91eda029e24a6f5e2b75929532b5a58d82
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-03-13 20:00:33 -0600

    Don't show nested drawings for plan files right now

--- app/controllers/plan_files_controller.rb
+++ app/controllers/plan_files_controller.rb
@@ -1,9 +1,10 @@
 class PlanFilesController < ApplicationController
   active_scaffold :plan_file do |config|
 		config.label = "Plan Files"
-    config.list.columns = [:plan_number, :location, :size, :title, :drawings]
+    config.columns = [:plan_number, :location, :size, :title, :drawings]
     config.columns[:plan_number].set_link(:edit)
-    config.update.columns.exclude [:location]
+    config.update.columns.exclude [:location, :drawings]
     config.list.always_show_search = true
+    config.delete.link = false
   end
 end 
