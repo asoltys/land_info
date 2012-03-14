28f3471152d744b0447bbb9cdc03620ff634fdcc
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-03-13 22:03:51 -0600

    List plan files more cleanly

--- app/helpers/survey_files_helper.rb
+++ app/helpers/survey_files_helper.rb
@@ -1,2 +1,12 @@
 module SurveyFilesHelper
+  def survey_file_plan_files_column(record)
+    if record.plan_files.any?
+      str = record.plan_files.first(3).collect{|p| p.plan_number}.join(', ')
+      str += '...' if record.plan_files.count > 3
+      return str
+    else
+      # This way there's something to turn into a link if there are no roles associated with this record yet.
+      active_scaffold_config.list.empty_field_text
+    end
+  end
 end
