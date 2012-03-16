3066d95df587a3cb2fa3d2fc75a8103e3a0a25de
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-03-16 16:27:54 -0600

    Remove tests and specs

--- test/unit/helpers/survey_files_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SurveyFilesHelperTest < ActionView::TestCase
-end
--- test/unit/survey_file_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class SurveyFileTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
