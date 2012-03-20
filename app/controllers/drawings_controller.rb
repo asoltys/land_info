75f5b8b89d7746f8e6cca3453a0a9b0e9422f68d
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-03-20 10:24:57 -0600

    Add more drawings columns

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,14 +1,15 @@
 class DrawingsController < ApplicationController
   active_scaffold :drawing do |config|
     
-    config.columns = [:tiff_file]
-
-    config.update.columns.exclude [
-      :drawing_file_name, 
-      :drawing_content_type, 
-      :drawing_file_size, 
-      :drawing_updated_at
+    config.columns = [
+      :tiff_file 
     ]
+
+    config.list.columns = [:tiff_file, :drawing_file_name, :drawing_file_size]
+
+    config.columns[:tiff_file].label = "Number"
+    config.columns[:drawing_file_name].label = "File name"
+    config.columns[:drawing_file_size].label = "Size"
 
     config.columns[:drawing_file_name].set_link(:edit)
   end
