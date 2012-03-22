a46a88929f5a5d5a0d13c2bcd76153e111929cc2
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-03-21 21:57:30 -0600

    Fix date column migration

--- db/migrate/20120225003229_change_date_fields.rb
+++ db/migrate/20120225003229_change_date_fields.rb
@@ -1,13 +1,17 @@
 class ChangeDateFields < ActiveRecord::Migration
   def up
-    execute <<-SQL
+    sql = <<-SQL
       UPDATE survey_files SET 
         start_date = date(str_to_date(start_date, '%m/%d/%Y')),
-        completion_date = date(str_to_date(completion_date, '%m/%d/%Y'))
+        completion_date = date(str_to_date(completion_date, '%m/%d/%Y'));
+
+      ALTER TABLE survey_files MODIFY COLUMN start_date date;
+      ALTER TABLE survey_files MODIFY COLUMN completion_date date;
     SQL
 
-    change_column :survey_files, :start_date, :date
-    change_column :survey_files, :completion_date, :date
+    sql.strip.split(';').each do |s| 
+      ActiveRecord::Base.connection.execute(s) 
+    end 
   end
 
   def down
