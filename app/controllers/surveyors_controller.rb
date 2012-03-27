aab81c305b07b1630349a088da869a380629dfec
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-03-27 17:43:23 -0600

    Allowing sorting surveyors by name

--- app/controllers/surveyors_controller.rb
+++ app/controllers/surveyors_controller.rb
@@ -1,6 +1,8 @@
 class SurveyorsController < ApplicationController
   active_scaffold :surveyor do |config|
+    config.columns << :name
     config.list.columns = [:name, :company, :city, :street_address]
+    config.columns[:name].sort_by :sql => ['first_name', 'last_name']
     config.search.link = false
     config.update.link = false
     config.actions.exclude :show
